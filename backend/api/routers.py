b716a0df17922e4e2f4db8ca6724e8cd067dbd22
Jwdev080 <148032109+Jwdev080@users.noreply.github.com>
2024-07-12 15:22:19 +0200

    Update routers.py

--- backend/api/routers.py
+++ backend/api/routers.py
@@ -8,15 +8,10 @@ from rest_framework import routers
 from api.user.viewsets import UserViewSet
 
 router = routers.SimpleRouter(trailing_slash=False)
-
 router.register(r"edit", UserViewSet, basename="user-edit")
-
 router.register(r"register", RegisterViewSet, basename="register")
-
 router.register(r"login", LoginViewSet, basename="login")
-
 router.register(r"checkSession", ActiveSessionViewSet, basename="check-session")
-
 router.register(r"logout", LogoutViewSet, basename="logout")
 
 urlpatterns = [
